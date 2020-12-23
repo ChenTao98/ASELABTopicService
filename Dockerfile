6ea2c0eadd056af9cc4d225f1223c256489ef334
Chen <384490967@qq.com>
2020-12-23 21:23:02 +0800

    dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM java:8
 COPY "./target/topic-0.0.1-SNAPSHOT.jar" "/topic-0.0.1-SNAPSHOT.jar"
-EXPOSE 21002
+EXPOSE 8082
 CMD ["java", "-jar","topic-0.0.1-SNAPSHOT.jar","--spring.profiles.active=product"]
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn clean package -DskipTests
 docker build -t aselab:topic .
-docker run -p 21002:21002 -d --name aselabtopicservice aselab:topic
+docker run -p 8082:8082 -d --name aselabtopicservice aselab:topic
